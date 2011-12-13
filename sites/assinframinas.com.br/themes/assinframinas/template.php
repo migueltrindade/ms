285b5a065f67779f02d3a8124974fb807efbd72d
Miguel Trindade <migueltrindadejr@gmail.com>
2011-12-13 21:46:40 -0200

    novo site

---
+++ sites/assinframinas.com.br/themes/assinframinas/template.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Implements hook_preprocess_maintenance_page().
+ */
+function assinframinas_preprocess_maintenance_page(&$vars) { 
+  unset($vars['site_name']);
+  unset($vars['site_slogan']);
+  unset($vars['title']);
+  drupal_add_css(drupal_get_path('theme', 'assinfra') . '/css/maintenance.css');
+}

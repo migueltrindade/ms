6da00074ae7df151d073d0bb09af399463d77b45
Miguel Trindade <migueltrindadejr@gmail.com>
2011-12-14 23:36:46 -0200

    corrigindo tema

--- sites/assinframinas.com.br/themes/assinframinas/template.php
+++ sites/assinframinas.com.br/themes/assinframinas/template.php
@@ -7,5 +7,5 @@ function assinframinas_preprocess_maintenance_page(&$vars) {
   unset($vars['site_name']);
   unset($vars['site_slogan']);
   unset($vars['title']);
-  drupal_add_css(drupal_get_path('theme', 'assinfra') . '/css/maintenance.css');
+  drupal_add_css(drupal_get_path('theme', 'assinframinas') . '/css/maintenance.css');
 }
